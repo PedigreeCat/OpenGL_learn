81715ee021011935824e1f22bc0108e2def1f7f5
PedigreeCat <pedigreecat@qq.com>
2022-06-19 21:35:13 +0800

    使用VAO绘制一个三角形

--- hello_window/src/main.cpp
+++ hello_window/src/main.cpp
@@ -2,6 +2,22 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+#define LOG_SIZE (512)
+
+const char* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
+    "}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     // Viewport
@@ -23,7 +39,9 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // 主版本号
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // 子版本号
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 使用核心模式
-    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Mac OS X系统需要添加这行代码
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Mac OS X系统需要添加这行代码
+#endif
     
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
@@ -33,6 +51,8 @@ int main()
         return -1;
     }
     glfwMakeContextCurrent(window);
+    // 窗口大小调整时回调函数
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // GLAD
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -41,27 +61,98 @@ int main()
         return -1;
     }
 
-    // 窗口大小调整时回调函数
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    // Shader
+    unsigned int vertexShader;
+    int  success;
+    char infoLog[LOG_SIZE];
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, LOG_SIZE, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
 
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, LOG_SIZE, NULL, infoLog);
+        std::cout << "ERROR:SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
 
+    // Shader Program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, LOG_SIZE, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAME::LINK_FAILED\n" << infoLog << std::endl;
+    }
+    // 着色器对象连接到程序后就不需要了
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    // VAO
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // VBO
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // VertexAttrib
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // glVertexAttribPointer已经将VBO注册为了顶点属性的绑定VBO，在那之后我们可以安全的进行unbind
+    // 也就是说这里解绑也没关系，VBO已经绑定到VAO了
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    // 你可以在以后将VAO解绑，以防止其他VAO意外的修改了当前的VAO，不过这种情况很少发生
+    glBindVertexArray(0);
+    
     // Render Loop
     while (!glfwWindowShouldClose(window))
     {
-        // input
+        // deal with input
         processInput(window);
 
-        // render instruct
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
+        // render instruct
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+
     }
 
     // Final
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
     glfwTerminate();
 
     return 0;
